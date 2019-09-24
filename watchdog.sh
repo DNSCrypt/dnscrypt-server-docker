cd8f720cf6bb34517df63d7566258ba17890c37d
Frank Denis <github@pureftpd.org>
2019-09-24 22:59:40 +0200

    Re-drop privileges

--- watchdog.sh
+++ watchdog.sh
@@ -5,11 +5,3 @@ sleep 300
 for service in unbound encrypted-dns; do
     sv check "$service" || sv force-restart "$service"
 done
-
-KEYS_DIR="/opt/encrypted-dns/etc/keys"
-GRACE_PERIOD=60
-
-provider_name=$(cat "${KEYS_DIR}/provider_name")
-
-drill -p 443 -Q TXT "$provider_name" @127.0.0.1 ||
-    sv force-restart encrypted-dns
